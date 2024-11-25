120a474a8b31b384262f9856b2da42d7b6718478
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-25 19:27:47 +0600

    Implemented the MVC code

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,19 @@
 const makeAPICall = async (url) => {
     try {
         const response = await fetch(url);
-        if(response.ok) {
+        if (response.ok) {
             const data = response.json();
             return data
         } else {
             console.error(`Error fetching data: ${response.error}`);
         }
-    } catch(e) {
+    } catch (e) {
         return e;
     }
 }
 
 const cleanResponseData = (data) => {
-    if(data.length === 0) {
+    if (data.length === 0) {
         return null;
     }
     return data.map(elem => {
@@ -24,10 +24,50 @@ const cleanResponseData = (data) => {
     });
 }
 
-(async() => {
-    const URL = "https://excuser-three.vercel.app/v1/excuse/developers/4";
-    const responseData = await makeAPICall(URL);
-    const execuses = cleanResponseData(responseData);
+// (async() => {
+//     const URL = "https://excuser-three.vercel.app/v1/excuse/developers/4";
+//     const responseData = await makeAPICall(URL);
+//     const execuses = cleanResponseData(responseData);
 
-    console.log(execuses);
-})();
+//     console.log(execuses);
+// })();
+
+const Model = {
+    state: {
+        excuses: []
+    },
+
+    async fetchData(URL) {
+        try {
+            const response = await fetch(URL);
+            if(!response.ok) {
+                throw new Error("Failed to fetch excuses");
+            }
+            const data = await response.json();
+            this.state.excuses = data.map(elem => {
+                return {
+                    excuse: elem.excuse,
+                    category: elem.category
+                }
+            })
+        } catch (error) {
+            this.state.excuses = "Error fetching joke";
+            console.error(error);
+        }
+        this.notify();
+    },
+    observers: [],
+    subscribe(observer) {
+        this.observers.push(observer);
+    },
+
+    notify() {
+        this.observers.forEach(observer => observer());
+    }
+};
+
+const View = {
+    init() {
+
+    }
+}
